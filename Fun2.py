93cb63ef7f88132f6a36266a5d4f2deeb1bb6eb9
adan19-meet <adan19@meet.mit.edu>
2017-07-30 11:48:31 +0300

    Stopping here

--- Fun2.py
+++ Fun2.py
@@ -1,8 +1,6 @@
-print('*')
-print('*')
-print('*')
-print('*')
-print('*')
-print('*****')
-def draw_1d():
-    
+c1 = '~'
+c2= '!' 
+def draw_1d (n , m ,char):
+    for n in range(m):
+return(char * n * m )
+
